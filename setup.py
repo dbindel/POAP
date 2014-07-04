a4ee1b7c0dc01785ddf4f5a4e88c85bfb7988d45
David Bindel <bindel@cs.cornell.edu>
2014-07-04 16:52:09 -0400

    Bumped version number for POAP.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='POAP',
-    version='0.1.0',
+    version='0.1.1',
     author='David Bindel',
     author_email='bindel@cornell.edu',
     packages=['poap', 'poap.test'],
