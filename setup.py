66c8c225e4de187c5095e3b86c334e5d63b9333d
David Bindel <bindel@cs.cornell.edu>
2015-05-13 09:48:35 -0400

    Updates to version information.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='POAP',
-    version='0.1.1',
+    version='0.1.2',
     author='David Bindel',
     author_email='bindel@cornell.edu',
     packages=['poap', 'poap.test'],
