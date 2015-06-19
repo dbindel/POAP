4abd84308ada07ddb475a35ab36be2c377db43f3
David Bindel <bindel@cs.cornell.edu>
2015-06-19 18:06:21 -0400

    Update to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='POAP',
-    version='0.1.4',
+    version='0.1.5',
     author='David Bindel',
     author_email='bindel@cornell.edu',
     packages=['poap', 'poap.test'],
