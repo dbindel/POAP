3336f8db92899ee5998ec15c0c4b7732961fcee0
David Bindel <bindel@cs.cornell.edu>
2015-06-20 21:08:50 -0400

    Tests for PromiseStrategy and ThreadStrategy.

--- poap/test/test_strategies.py
+++ poap/test/test_strategies.py
@@ -4,7 +4,8 @@ from poap.controller import ScriptedController
 from poap.strategy import FixedSampleStrategy
 from poap.strategy import CoroutineStrategy
 from poap.strategy import CoroutineBatchStrategy
-# from poap.strategy import ThreadStrategy
+from poap.strategy import PromiseStrategy
+from poap.strategy import ThreadStrategy
 # from poap.strategy import OptimizerThread
 # from poap.strategy import CheckWorkerStrategy
 # from poap.strategy import SimpleMergedStrategy
@@ -151,6 +152,68 @@ def test_coroutine_batch3():
     c.terminate()
 
 
+def test_promise1():
+    "Check promise batch strategy"
+    c = ScriptedController()
+    s = PromiseStrategy()
+    c.strategy = s
+
+    # Start promise and check not ready
+    p = s.promise_eval(1)
+    assert not p.ready()
+
+    # Give the eval a hard time before completing
+    c.reject_eval(args=(1,))
+    r = c.accept_eval(args=(1,))
+    r.kill()
+    r = c.accept_eval(args=(1,))
+    assert not p.ready()
+    r.complete(1)
+
+    # Check to make sure the value came through
+    assert p.value == 1
+
+    # Terminate
+    s.terminate()
+    c.reject_terminate()
+    c.reject_terminate()
+    c.accept_terminate()
+    c.terminate()
+
+
+def thread_optimizer(strategy, vs, v):
+    assert vs == strategy.blocking_evals(vs)
+    assert v == strategy.blocking_eval(v)
+
+
+def test_thread1():
+    "Check promise batch strategy"
+    c = ScriptedController()
+    c.strategy = ThreadStrategy(c, lambda s: thread_optimizer(s, [1, 2], 3))
+
+    # Make some trouble for the first two points
+    r1 = c.accept_eval(args=(1,), skip=True)
+    c.reject_eval(args=(2,), skip=True)
+    r2 = c.accept_eval(args=(2,), skip=True)
+    c.no_proposal()
+    r1.kill()
+    r1 = c.accept_eval(args=(1,), skip=True)
+    c.no_proposal()
+    r1.complete(1)
+    r2.complete(2)
+
+    # Let the third proposal go through easy
+    r3 = c.accept_eval(args=(3,), skip=True)
+    c.no_proposal()
+    r3.complete(3)
+
+    # Reject two termination messages before we quit
+    c.reject_terminate(skip=True)
+    c.reject_terminate(skip=True)
+    c.accept_terminate(skip=True)
+    c.terminate()
+
+
 def main():
     test_fixed_sample1()
     test_fixed_sample2()
@@ -159,6 +222,8 @@ def main():
     test_coroutine_batch1()
     test_coroutine_batch2()
     test_coroutine_batch3()
+    test_promise1()
+    test_thread1()
 
 
 if __name__ == "__main__":
