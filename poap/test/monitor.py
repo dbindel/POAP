92b0153443ce671751e1d352c66b51e5a30d4877
David Bindel <bindel@cs.cornell.edu>
2015-06-23 14:42:08 -0400

    Added logging monitor.

---
+++ poap/test/monitor.py
@@ -0,0 +1,25 @@
+"""
+Add periodic logging to a controller.
+"""
+
+import logging
+
+
+# Module logger
+logger = logging.getLogger(__name__)
+
+
+def add_monitor(controller, timeout):
+    "Add a monitoring logger"
+
+    def monitor():
+        "Report progress of the optimization, roughly once a second."
+        record = controller.best_point()
+        if record:
+            logger.info("Best point {0} @ {1}".format(
+                record.value, record.params))
+        else:
+            logger.info("No points yet")
+        controller.add_timer(timeout, monitor)
+
+    controller.add_timer(timeout, monitor)
