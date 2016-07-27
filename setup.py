8056e86c003024e94cc07c76397ea302d656670b
David Bindel <bindel@cs.cornell.edu>
2016-07-27 12:31:22 -0400

    Clean up from linter.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='POAP',
-    version='0.1.22',
+    version='0.1.23',
     author='David Bindel',
     author_email='bindel@cornell.edu',
     packages=['poap', 'poap.test'],
